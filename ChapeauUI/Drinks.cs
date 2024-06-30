356ec210132a529ed064ad5722678ec33f7c3753
TeletabiTrifasico <hugojiba@gmail.com>
2024-06-30 17:02:20 +0200

    fixed to UI

---
+++ ChapeauUI/Drinks.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChapeauUI._2
+{
+    public partial class Drinks : Form
+    {
+        public Drinks()
+        {
+            InitializeComponent();
+        }
+        private void OpenHomePage()
+        {
+            HomePage homePage = new();
+            homePage.Show();
+            this.Hide();
+        }
+        private void OpenStockPage()
+        {
+            Stock stock = new();
+            stock.Show();
+            this.Hide();
+        }
+        private void OpenEmployeePage()
+        {
+            Employees employees = new();
+            employees.Show();
+            this.Hide();
+        }
+
+        private void homeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenHomePage();
+        }
+        private void stockToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenStockPage();
+        }
+
+        private void employeesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenEmployeePage();
+        }
+    }
+}
---
+++ ChapeauUI/MenuItems.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChapeauUI._2
+{
+    public partial class MenuItems : Form
+    {
+        public MenuItems()
+        {
+            InitializeComponent();
+        }
+
+        private void homeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenHomePage();
+        }
+        private void OpenHomePage()
+        {
+            HomePage homePage = new();
+            homePage.Show();
+            this.Hide();
+        }
+        private void stockToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenStockPage();
+        }
+        private void OpenStockPage()
+        {
+            Stock stock = new();
+            stock.Show();
+            this.Hide();
+        }
+        private void employeesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenEmployeePage();
+        }
+        private void OpenEmployeePage()
+        {
+            Employees employees = new();
+            employees.Show();
+            this.Hide();
+        }
+        private void OpenDrinksPage()
+        {
+            Drinks drinks = new();
+            drinks.Show();
+            this.Hide();
+        }
+        private void OpenFoodPage()
+        {
+            Food food = new();
+            food.Show();
+            this.Hide();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenDrinksPage();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFoodPage();
+        }
+    }
+}
